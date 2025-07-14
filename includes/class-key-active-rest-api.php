d4621954e376b3f67c37caa6c4967f29b33b4f2e
Danh Trọng Trần <danhtrongtran@Danhs-MacBook-Air.local>
2025-07-15 06:28:45 +0700

    Cập nhật - 2025-07-15 06:28:45

--- includes/class-key-active-rest-api.php
+++ includes/class-key-active-rest-api.php
@@ -1,117 +1,131 @@
 <?php
-add_action( 'rest_api_init', function () {
-    register_rest_route(
-        'license/v2',
-        '/check',
-        array(
-            'methods'  => 'GET',
-            'callback' => 'check_key_active',
-        )
-    );
-    } );
 
+class Key_Active_REST_API {
 
-function check_key_active(WP_REST_Request $request)
-    {
-    global $wpdb;
-
-    $License = $request->get_param( 'License' );
-    $Domain  = $request->get_param( 'Domain' );
-    $Soft_Id = $request->get_param( 'Soft_Id' );
-    $data    = [
-        "error"   => 100,
-        "success" => true,
-        "msg"     => "License: " . $License . " - Hợp lệ",
-    ];
-
-    $post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type='key_active'", $License ) );
-
-    if ( $post ) {
-        $active_key = get_post( $post, OBJECT );
-        $meta_data  = get_post_meta( $active_key->ID ); // Retrieve all post meta
-
-        $domain      = isset($meta_data['domain'][0]) 		? $meta_data['domain'][0] : '';
-        $plugin_id   = isset($meta_data['plugin_id'][0]) 	? $meta_data['plugin_id'][0] : '';
-        $plugin_name = isset($meta_data['plugin_name'][0]) 	? $meta_data['plugin_name'][0] : '';
-        $version	 = isset($meta_data['version'][0]) 		? $meta_data['version'][0] : '';
-		
-        $status      = $meta_data['status'][0];
-        $expire      = $meta_data['expire'][0];
-
-        if ( $plugin_id !== $Soft_Id ) {
-            $data = [
-                "error"   => 101,
-                "success" => false,
-                "msg"     => "Plugin không hợp lệ.",
-            ];
-            return rest_ensure_response( $data );
-            }
-
-        if ( $domain !== $Domain ) {
-            $data = [
-                "error"   => 102,
-                "success" => false,
-                "msg"     => "Tên miền không hợp lệ.",
-            ];
-            return rest_ensure_response( $data );
-            }
-
-		if ( $status == 'true' ) {
-			$status = "Active";
-		} else {
-			$status = "Inactive";
-			 $data = [
-                "error"   => 102,
-                "success" => false,
-                "msg"     => "Bị vô hiệu hoá rùi! Liên hệ Zalo: <strong style='color:green'> 0813.908.901 </strong>.",
-            ];
-            return rest_ensure_response( $data );
-		}
-        $date     = '';
-        $isExpire = !isset($meta_data['expire']) || $meta_data['expire'][0] == '';
-
-        if ( $isExpire ) {
-            $date = date_create();
-            date_add( $date, date_interval_create_from_date_string( "999 years" ) );
-            } else {
-            $date = date_create( $meta_data['expire'][0] );
-            }
-
-        $plugin = [
-            "row"             => $active_key->ID,
-            "ID"              => $plugin_id,
-            "Name"            => $plugin_name,
-            "Price"           => 0,
-            "Download_Url"    => "",
-            "Change_Log"      => "",
-            "Newest_Version"  => $version,
-            "Update New Core" => "",
-        ];
-
-        $license         = [
-            "row"            => $active_key->ID,
-            "License"        => $License,
-            "Soft_Id"        => $plugin_id,
-            "Soft_Name"      => $plugin_name,
-            "Price"          => "",
-            "Domain"         => $domain,
-            "Expiry_Date"    => $expire ? date( 'd-m-Y', strtotime( $expire ) ) : '',
-            "Customer_Name"  => "Tran Danh Trong",
-            "Customer_Phone" => "0813908901",
-            "Customer_Email" => "codevnes@gmail.com",
-            "Status"         => $status,
-            "Expiry_C_Use"   => "",
-            "Timestamp"      => $isExpire ? '' : $date->getTimestamp(),
-            "Note"           => "",
-        ];
-        $data['plugin']  = $plugin;
-        $data['license'] = $license;
-        } else {
-        $data = [
-            "error"   => 103,
-            "success" => false,
-            "msg"     => "License:" . $License . " -  không hợp lệ",
-        ];
-        }
-    return rest_ensure_response( $data );
+    public function __construct() {
+        add_action( 'rest_api_init', array( $this, 'register_routes' ) );
     }
+
+    public function register_routes() {
+        register_rest_route(
+            'license/v2',
+            '/check',
+            array(
+                'methods'             => 'GET',
+                'callback'            => array( $this, 'check_key_active' ),
+                'permission_callback' => '__return_true',
+            )
+        );
+    }
+
+    public function check_key_active(WP_REST_Request $request)
+    {
+        global $wpdb;
+
+        $License = $request->get_param( 'License' );
+        $Domain  = $request->get_param( 'Domain' );
+        $Soft_Id = $request->get_param( 'Soft_Id' );
+        
+        // Dữ liệu trả về mặc định
+        $data    = [
+            "error"   => 0,
+            "success" => true,
+            "msg"     => "Giấy phép: " . $License . " - Hợp lệ",
+        ];
+
+        $post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type='key_active'", $License ) );
+
+        if ( $post ) {
+            $active_key = get_post( $post, OBJECT );
+            $meta_data  = get_post_meta( $active_key->ID );
+
+            $domain      = isset($meta_data['domain'][0]) ? $meta_data['domain'][0] : '';
+            $plugin_id   = isset($meta_data['plugin_id'][0]) ? $meta_data['plugin_id'][0] : '';
+            $plugin_name = isset($meta_data['plugin_name'][0]) ? $meta_data['plugin_name'][0] : '';
+            $version	 = isset($meta_data['version'][0]) ? $meta_data['version'][0] : '';
+            $status      = isset($meta_data['status'][0]) ? $meta_data['status'][0] : 'false';
+            $expire      = isset($meta_data['expire'][0]) ? $meta_data['expire'][0] : '';
+
+            if ( $plugin_id !== $Soft_Id ) {
+                $data = [
+                    "error"   => 101,
+                    "success" => false,
+                    "msg"     => "Plugin không hợp lệ.",
+                ];
+                return rest_ensure_response( $data );
+            }
+
+            if ( $domain !== $Domain ) {
+                $data = [
+                    "error"   => 102,
+                    "success" => false,
+                    "msg"     => "Tên miền không hợp lệ.",
+                ];
+                return rest_ensure_response( $data );
+            }
+
+            if ( $status !== 'true' ) {
+                 $data = [
+                    "error"   => 104, // Sửa mã lỗi trùng lặp
+                    "success" => false,
+                    "msg"     => "Bị vô hiệu hoá! Liên hệ Zalo: <strong style='color:green'> 0813.908.901 </strong>.",
+                ];
+                return rest_ensure_response( $data );
+            }
+            
+            $isExpire = !isset($meta_data['expire']) || $meta_data['expire'][0] == '';
+
+            if ( !$isExpire && time() > strtotime($expire) ) {
+                $data = [
+                    "error"   => 105,
+                    "success" => false,
+                    "msg"     => "Giấy phép đã hết hạn.",
+                ];
+                return rest_ensure_response( $data );
+            }
+
+            $plugin = [
+                "row"             => $active_key->ID,
+                "ID"              => $plugin_id,
+                "Name"            => $plugin_name,
+                "Price"           => 0,
+                "Download_Url"    => "",
+                "Change_Log"      => "",
+                "Newest_Version"  => $version,
+                "Update New Core" => "",
+            ];
+
+            $license_status = ($status === 'true') ? 'Active' : 'Inactive';
+            $expiry_date_formatted = $expire ? date( 'd-m-Y', strtotime( $expire ) ) : 'Vĩnh viễn';
+
+            $license = [
+                "row"            => $active_key->ID,
+                "License"        => $License,
+                "Soft_Id"        => $plugin_id,
+                "Soft_Name"      => $plugin_name,
+                "Price"          => "",
+                "Domain"         => $domain,
+                "Expiry_Date"    => $expiry_date_formatted,
+                "Customer_Name"  => isset($meta_data['person'][0]) ? $meta_data['person'][0] : "Tran Danh Trong",
+                "Customer_Phone" => "0813908901",
+                "Customer_Email" => isset($meta_data['email'][0]) ? $meta_data['email'][0] : "codevnes@gmail.com",
+                "Status"         => $license_status,
+                "Expiry_C_Use"   => "",
+                "Timestamp"      => $expire ? strtotime($expire) : '',
+                "Note"           => "",
+            ];
+            
+            $data['plugin']  = $plugin;
+            $data['license'] = $license;
+            
+        } else {
+            $data = [
+                "error"   => 103,
+                "success" => false,
+                "msg"     => "Giấy phép: " . $License . " - không hợp lệ",
+            ];
+        }
+        
+        return rest_ensure_response( $data );
+    }
+}
